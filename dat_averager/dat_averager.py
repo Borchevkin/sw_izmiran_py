c1f375cc973e219edbda1ae2d4ef70f5982d6746
Danil Borchevkin <danil.borchevkin@gmail.com>
2024-11-07 01:14:34 +0300

    Fix issue. Fix code formatting

--- dat_averager/dat_averager.py
+++ dat_averager/dat_averager.py
@@ -1,21 +1,29 @@
 from izmiran.datfile import *
 
+
 def main():
-    input_filename = "./input/109705_25.dat"    # Input filename
-    output_filename = "./output/output.dat"  # Output filename
-    start_idx = 0   # Start index
-    end_idx = 10    # End index. Used half-interval approach, so interval is [0; 10)
-    period = 5      # Peroid of averaging
+    INPUT_FILENAME = "./input/109705_25.dat"  # Input filename
+    OUTPUT_FILENAME = "./output/output.dat"  # Output filename
+    FILTER_COLUMND_IDX = 0  # Column index of filtered data
+    FILTER_MIN_VAL = 90.0  # Minimum value of filtered data
+    FILTER_MAX_VAL = 100.0  # Maximum value of filtered data
+    AVG_START_IDX = 0  # Start index
+    AVG_END_IDX = 10  # End index. Used half-interval approach, so interval is [0; 10)
+    AVG_PERIOD = 5  # Period of averaging
 
     in_datfile = DatFile()
-    in_datfile.read(input_filename)
-    
-    # NOTE: it's specific only for v0.1.0 izmiran module.
-    # NOTE: reverse field directly
-    in_datfile.data_list_of_records.reverse()
+    in_datfile.read(INPUT_FILENAME)
 
-    out_datfile = in_datfile.avg(period, start_idx=start_idx, end_idx=end_idx)
-    out_datfile.write(output_filename)
+    reversed_datfile = in_datfile.reverse()
+    filtered_datfile = reversed_datfile.filter(column_idx=FILTER_COLUMND_IDX,
+                                               min_val=FILTER_MIN_VAL,
+                                               max_val=FILTER_MAX_VAL)
+    out_datfile = filtered_datfile.avg(AVG_PERIOD,
+                                       start_idx=AVG_START_IDX,
+                                       end_idx=AVG_END_IDX)
+
+    out_datfile.write(OUTPUT_FILENAME)
+
 
 if __name__ == '__main__':
     main()
